0f05618155b9f60c4b909352274a97dca35e1719
Sarah Collins <smc322@cornell.edu>
2017-11-03 09:53:21 -0500

    add chosen covariates to WI data, save a file with WI dataset

--- R/AssociateLAGOSwData.R
+++ R/AssociateLAGOSwData.R
@@ -1,4 +1,4 @@
-##want to associate data 
+##want to associate data from LAGOS to WI and IA data - match lakes with lagoslakeid, then associate with covariates of choice (see details below)
 
 wi<- readRDS(file="Data/LTERNitrogenData.rds")
 ia<-readRDS(file="Data/IowaNitrogenData.rds")
@@ -59,7 +59,60 @@ names(lter.codes) = c("lakeid", "lagoslakeid")
 
 wi.lagoslakeid<-merge(wi, lter.codes, by="lakeid", all.x=T, all.y=T)
 
-###geo variables to include -- rowcrop iws, pasture iws, no3 deposition, 
+###geo variables to include per SMC discussion with EHS on 31 Oct 17 -- NO3 and TN depo, Runoff, Baseflow (all of these at hu8), chla (to indicate how green the lake is, could also add P or secchi in future?), IWS level LULC for urban (sum), wetland (sum), ag (row crop and pasture separate), forest (sum), open water, lake depth, LA:WA, connectivity class.
+#mean/median year for WI data is 2000/2001 so used 2000 dep data and 2001 nlcd
+
+hu.8id<-locus[,c(1,13)]
+
+wi.lakeid.hu8<-merge(wi.lagoslakeid, hu.8id, all.x=T, all.y=F)
+
+hu8.chag<-lagos$hu8.chag
+hu8.chag.rel<-hu8.chag[,c(1,4, 20, 68, 100)]
+
+wi.chag<-merge(wi.lakeid.hu8, hu8.chag.rel, by="hu8_zoneid", all.x=T, all.y=F)
+
+iws.lulc<-lagos$iws.lulc
+iws.lulc.rel<-iws.lulc[,c(52, 54, 56, 58, 60, 64, 66, 68, 74, 76, 78, 80, 156)]
+iws.lulc.rel$urban<-iws.lulc.rel$iws_nlcd2001_pct_21+iws.lulc.rel$iws_nlcd2001_pct_22+iws.lulc.rel$iws_nlcd2001_pct_23+iws.lulc.rel$iws_nlcd2001_pct_24
+iws.lulc.rel$rowcrop<-iws.lulc.rel$iws_nlcd2001_pct_82
+iws.lulc.rel$pasture<-iws.lulc.rel$iws_nlcd2001_pct_81
+iws.lulc.rel$forest<-iws.lulc.rel$iws_nlcd2001_pct_41+iws.lulc.rel$iws_nlcd2001_pct_42+iws.lulc.rel$iws_nlcd2001_pct_43
+iws.lulc.rel$wetland<-iws.lulc.rel$iws_nlcd2001_pct_90+iws.lulc.rel$iws_nlcd2001_pct_95
+iws.lulc.rel$openh20<-iws.lulc.rel$iws_nlcd2001_pct_11
+  
+iws.lulc.rel.keep<-iws.lulc.rel[,c(13:19)]
+iws.lulc.rel.keep.nona<-na.omit(iws.lulc.rel.keep)
+
+wi.chag.lulc<-merge(wi.chag, iws.lulc.rel.keep.nona, by="lagoslakeid", all.x=T, all.y=F)
+
+##note NAs -- trout bog is only ~1ha so is not part of the ~50,000 lakes in the IWS dataset.  Could add HU12 lulc data if needed?
+
+area<-locus[,c(1,6)]
+wsarea<-lagos$iws[,c(2,12)]
+maxd<-lagos$lakes_limno[,c(1,6)]
+connclass<-lagos$lakes.geo[,c(1,31)]
+
+lakewsa<-merge(area, wsarea, by="lagoslakeid", all.x=T, all.y=T)
+areadepth<-merge(lakewsa, maxd, by="lagoslakeid", all.x=T, all.y=T)
+areadepthconn<-merge(areadepth, connclass, by="lagoslakeid", all.x=T, all.y=T)
+areadepthconn$la_wa_ratio<-areadepthconn$lake_area_ha/areadepthconn$iws_ha
+
+chla<-lagos$epi_nutr[,c(2,7,92)]
+chla.80<-na.omit(chla[chla$sampleyear>1979,])
+
+annualmed = aggregate(chla.80$chla, chla.80[,c("lagoslakeid", "sampleyear")], FUN=function(x) c(median=median(x)))
+lakemed = aggregate(annualmed$x, by=list(annualmed$lagoslakeid), FUN=function(x) c(median=median(x)))
+names(lakemed)<-c("lagoslakeid", "median_chla")
+
+areadepthconnchla<-merge(areadepthconn, lakemed, by="lagoslakeid", all.x=T, all.y=T)
+
+local.covars<-areadepthconnchla[,c(1,4,5,6,7)]
+
+wi.chag.lulc.local<-merge(wi.chag.lulc, local.covars, by="lagoslakeid", all.x=T, all.y=F)
+
+##wi.chag.lulc.local is mostly what we want, with a couple of issues --- 4861 NAs for some variables because we don't have a lagoslakeid for crystal bog, and 13,154 NAs for some variables because trout bog is less than 4ha and excluded from IWS calculations for LAGOS (13154 should be sum of trout bog missing data and crystal bog missing data).  otherwise the data should be complete.  save as RDS here and explain issues to Erin and see if she has any other needs.
+
+saveRDS(wi.chag.lulc.local, file="Data/WIwithCovariates_3Nov2017.rds")
 
 
 ##iowa mess, continue this later
